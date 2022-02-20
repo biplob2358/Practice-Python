128fd5cc69142e627b7715212cabc5fa29da7e3a
BIPLOB <biplob2358@gmail.com>
2022-02-20 17:38:19 +0600

    update

---
+++ for loop.py
@@ -0,0 +1,11 @@
+# for i in range(11):
+#     print(i)
+#
+# for i in range(1,51,2):
+#     print(i0
+import time
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year")
---
+++ while loop.py
@@ -0,0 +1,6 @@
+name=""
+while len(name)==0:
+    name= input("Enter your Name:")
+print("Hello "+name)
+
+
