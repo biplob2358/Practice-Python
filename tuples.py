b6d64b44084ebbf7d833672df37a294df4bfe029
BIPLOB <biplob2358@gmail.com>
2022-02-21 19:57:33 +0600

    Add

---
+++ tuples.py
@@ -0,0 +1,27 @@
+name=("Biplob","Gourob","Masum","Sajib","Habib")
+print(name)
+name1=("Biplob","Biplob","Gourob","Masum","Sajib","Habib")
+print(name1)
+print(len(name))
+print(len(name1))
+print(type(name))
+print(name1[1])
+print(name1[-1])
+x=("Biplob","Gourob","Masum","Sajib","Habib")
+y=list(x)
+y.append("saju")
+print(y)
+
+
+
+
+
+
+# #str
+# h=("Biplob")
+# print(type(h))
+#
+# #tuple
+# F=("Biplob",)
+# print(type(F))
+
